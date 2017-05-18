f0729d3707cb67da360c530d74e2bde4557862df
uvblu <uvblu@LAPTOP-KOKCMQF8>
2017-05-18 15:31:31 +1000

    rewe

--- Assets/scripts/CheckerBoard.cs
+++ Assets/scripts/CheckerBoard.cs
@@ -4,74 +4,150 @@ using UnityEngine;
 
 public class CheckerBoard : MonoBehaviour
 {
-    public GameObject blackPic;
-    public GameObject whitePic;
-    public int boredX = 8, boredZ = 8;
-    public float picRad = 0.5f;
-    public Pic[,] pices;
-    private int halfBoardX, halfBoardZ;
-    private float picDie;
-    private Vector3 bottomLeft;
+    public string fileName;
+    public GameObject blackPiece;
+    public GameObject whitePiece;
 
-	void Start ()
+    public int boardX = 8, boardZ = 8;
+    public float pieceRadius = 0.5f;
+
+    public Piece[,] pieces;
+    private int halfBoardX, halfBoardZ;
+    private float pieceDiameter;
+    private Vector3 bottomLeft;
+    
+    // Use this for initialization
+    void Start()
     {
-        halfBoardX = boredX / 2;
-        halfBoardZ = boredZ / 2;
-        picDie = picRad * 2f;
+        // Calculate a few values
+        halfBoardX = boardX / 2;
+        halfBoardZ = boardZ / 2;
+        pieceDiameter = pieceRadius * 2f;
         bottomLeft = transform.position - Vector3.right * halfBoardX - Vector3.forward * halfBoardZ;
         CreateGrid();
-	}
+    }
 
     void CreateGrid()
     {
-        pices = new Pic[boredX, boredZ];
-        #region generate white pices
-        for(int x = 0; x < boredX; x += 2)
+        // Initialize the 2D array
+        pieces = new Piece[boardX, boardZ];
+
+        #region Generate White Pieces
+        // Loop through board columns and skip 2 each time
+        for(int x = 0; x < boardX; x += 2)
         {
+            // Loop through first 3 rows
             for(int z = 0; z < 3; z++)
             {
                 bool evenRow = z % 2 == 0;
+                // Ternary Operator
                 int gridX = evenRow ? x : x + 1;
                 int gridZ = z;
-                GereratePice(whitePic, gridX, gridZ);
+                // Generate the piece
+                GeneratePiece(whitePiece, gridX, gridZ);
             }
         }
         #endregion
 
-        #region gererate blacks
-        for (int x = 0; x < boredX; x += 2)
+        #region Generate Black Pieces
+        // Loop through board columns and skip 2 each time
+        for (int x = 0; x < boardX; x += 2)
         {
-            for (int z = boredZ - 3; z < boredZ; z++)
+            // Loop through last 3 rows
+            for (int z = boardZ - 3; z < boardZ; z++)
             {
                 bool evenRow = z % 2 == 0;
+                // Ternary Operator
                 int gridX = evenRow ? x : x + 1;
                 int gridZ = z;
-                GereratePice(blackPic, gridX, gridZ);
+                // Generate the piece
+                GeneratePiece(blackPiece, gridX, gridZ);
             }
         }
         #endregion
     }
 
-    void GereratePice(GameObject picePrefab, int x, int z)
+    void GeneratePiece(GameObject piecePrefab, int x, int z)
     {
-        GameObject clone = Instantiate(picePrefab);
+        // Create an instance of piece
+        GameObject clone = Instantiate(piecePrefab);
+        // Set the parent to be this transform
         clone.transform.SetParent(transform);
-        Pic pic = clone.GetComponent<Pic>();
-        PlacePice(pic, x, z); 
+        // Get the piece component from the clone
+        Piece piece = clone.GetComponent<Piece>();
+        // Place the piece
+        PlacePiece(piece, x, z);
     }
 
-    void PlacePice(Pic pice, int x, int z)
+    void PlacePiece(Piece piece, int x, int z)
     {
-        float xOffset = x * picDie + picRad;
-        float zOffset = z * picDie + picRad;
-        pice.gridX = x;
-        pice.gridZ = z;
-        pice.transform.position = bottomLeft + Vector3.right * xOffset + Vector3.forward * zOffset;
-        pices[x, z] = pice;
+
+        float xOffset = x * pieceDiameter + pieceRadius;
+        float zOffset = z * pieceDiameter + pieceRadius;
+        piece.gridX = x;
+        piece.gridZ = z;
+        // Move piece physically to board coordinate
+        piece.transform.position = bottomLeft + Vector3.right * xOffset + Vector3.forward * zOffset;
+        // Set piece in array slot
+        pieces[x, z] = piece;
+    }
+    
+    public void DropPiece(Piece piece, Vector3 position)
+    {
+        float percentX = (position.x + halfBoardX) / boardX;
+        float percentZ = (position.z + halfBoardZ) / boardZ;
+        percentX = Mathf.Clamp01(percentX);
+        percentZ = Mathf.Clamp01(percentZ);
+        int x = Mathf.RoundToInt((boardX - 1) * percentX);
+        int z = Mathf.RoundToInt((boardZ - 1) * percentZ);
+        if(IsValid(x,z))
+        {
+            Piece oldPiece = pieces[x, z];
+            if (oldPiece == null)
+            {
+                int oldX = piece.gridX;
+                int oldZ = piece.gridZ;
+                pieces[oldX, oldZ] = null;
+                PlacePiece(piece, x, z);
+            }
+            else
+            {
+                SwapPieces(piece, oldPiece);
+            }
+        }
+        else
+        {
+            int gridX = piece.gridX;
+            int gridZ = piece.gridZ;
+            PlacePiece(piece, gridX, gridZ);
+        }
     }
 
-    void Update ()
+    bool IsValid(int x,int z)
     {
-		
-	}
+        bool evenRow = z % 2 == 0;
+        bool evenColim = x % 2 == 0;
+        bool oddRow = z % 2 == 1;
+        bool oddColim = x % 2 == 1;
+        return (evenRow && evenColim) || (oddRow && oddColim);
+    }
+    
+    void SwapPieces(Piece a,Piece b)
+    {
+        if(a == null || b == null)
+        {
+            return;
+        }
+        int aX = a.gridX;
+        int aZ = a.gridZ;
+        int bX = b.gridX;
+        int bZ = b.gridZ;
+        PlacePiece(a, bX, bZ);
+        PlacePiece(b, aX, aZ);
+    }
+
+    void Update()
+    {
+
+    }
 }
